ffa392424fb868f851d0d53f0218d8ac8b95d9a9
Hackatack985 <72309893+Hackatack985@users.noreply.github.com>
2020-10-10 20:43:16 +0000

    Update fortnite.py

--- fortnite.py
+++ fortnite.py
@@ -122,7 +122,7 @@ client = commands.Bot(
     auth=fortnitepy.AdvancedAuth(
         email=data['email'],
         password=data['password'],
-        prompt_authorization_code=True,
+        prompt_authorization_code=False,
         delete_existing_device_auths=True,
         **device_auth_details
     ),
